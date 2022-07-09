278e9794161faeb0cea1118b6acfc5842757f593
ADOLFO SAN <104970935+Adolfo-crazy@users.noreply.github.com>
2022-07-09 04:04:23 -0500

    nv-menu.js

--- plugins/nv-menu.js
+++ plugins/nv-menu.js
@@ -4,12 +4,11 @@ import path from 'path'
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/menu.mp3'
-conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {
-type: 'audioMessage', 
+conn.sendFile(m.chat, vn, 'menuconn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio }).mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })
 ptt: true 
 })
 }
-handler.customPrefix = /menu/
+if (/^menu$/i.test(m.text)) {
 handler.command = new RegExp
 export default handler
 
