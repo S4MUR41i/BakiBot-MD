45b905b74215548e52852e8374aa6e04464c4f36
ADOLFO SAN <104970935+Adolfo-crazy@users.noreply.github.com>
2022-07-10 00:58:57 -0500

    main-jajaja.js

--- plugins/main-jajaja.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/57ed1c12ef1063150bf72.png', m, { packname: "sticker by", author: "𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫" })
-}
-
-handler.customPrefix = /^(jajaja|jaja|JAJAJA)$/i
-handler.command = new RegExp
-
-module.exports = handler
